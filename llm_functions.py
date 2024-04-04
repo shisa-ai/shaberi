f490e74074c137f7251c2b421fff534064e45a24
fukufuk <122790208+fukufuk@users.noreply.github.com>
2024-04-04 12:06:13 +0900

    merged openai & vLLM function

--- llm_functions.py
+++ llm_functions.py
@@ -4,18 +4,20 @@ from datasets import Dataset
 from openai import OpenAI
 
 
-def get_response_from_openai(messages:list, model_name:str) -> str:
+# === 評価生成関数群 ===
+def get_response_from_openai(messages: list, model_name: str) -> str:
     client = OpenAI(
         api_key=os.environ.get("OPENAI_API_KEY")
     )
     response = client.chat.completions.create(
-        messages = messages,
-        model = model_name,
-        temperature = 0,
+        messages=messages,
+        model=model_name,
+        temperature=0,
     )
     return response.choices[0].message.content
 
-def get_response_func(model_name: str):
+
+def get_response_func(model_name: str) -> callable:
     if "gpt" in model_name:
         return get_response_from_openai
     else:
@@ -23,32 +25,45 @@ def get_response_func(model_name: str):
         他のモデルで評価する場合は関数、分岐をここに追加
         """
         raise NotImplementedError(f"Model {model_name} is not supported")
-    
-def get_model_response(messages:list, model_name:str) -> str:
+
+
+def get_model_response(messages: list, model_name: str) -> str:
     answer_function = get_response_func(model_name)
     return answer_function(messages, model_name)
 
-def get_answer_from_openai(question:str, model_name:str) -> str:
-    return get_response_from_openai([{"role": "user", "content": question}], model_name)
 
-def get_answer_from_vllm(question:str, model_name:str) -> str:
-    api_key = "EMPTY"
-    api_base = "http://localhost:8000/v1"
+# === 回答生成関数群 ===
+def get_answer(question: str, model_name: str):
+    api_key = os.environ.get("OPENAI_API_KEY", "EMPTY")
+    if api_key == "EMPTY":
+        base_url = "http://localhost:8000/v1"
+    else:
+        base_url = None
+
     client = OpenAI(
         api_key=api_key,
-        base_url=api_base,
+        base_url=base_url,
     )
-    completion = client.completions.create(model=model_name,
-                                        prompt=question)
-    return completion.choices[0].text
 
-def get_answerer(model_name: str):
-    if "gpt" in model_name:
-        return get_answer_from_openai
-    else:
-        return get_answer_from_vllm
+    response = client.chat.completions.create(
+        messages=[{"role": "user", "content": question}],
+        model=model_name,
+        temperature=0,
+    )
+    return response.choices[0].message.content
 
-def get_model_answer(dataset: Dataset, model_name:str, batch_size: int) -> Dataset:
+
+def get_answerer(model_name: str) -> callable:
+    """OpenAIとvLLM以外のモデルを使う場合はここに追加する"""
+    return get_answer
+
+
+def get_model_answer(dataset: Dataset,
+                     model_name: str,
+                     batch_size: int) -> Dataset:
     answer_function = get_answerer(model_name)
-    dataset = dataset.map(lambda x: {"ModelAnswer": answer_function(x['Question'], model_name)}, num_proc=batch_size)
+    dataset = dataset.map(
+        lambda x: {"ModelAnswer": answer_function(x['Question'], model_name)},
+        num_proc=batch_size
+    )
     return dataset
