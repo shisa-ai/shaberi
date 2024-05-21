884bd0a288728efc04f37a2d07daa7054f9a20fd
lhl <lhl@randomfoo.net>
2024-05-21 11:55:10 +0000

    test scripts

--- 01-answer.sh
+++ 01-answer.sh
@@ -1,7 +1,7 @@
-OPENAI_BASE_URL='http://localhost:8000/v1' python generate_answers.py --model_name 'augmxnt/shisa-gamma-7b-v1.rp1.15' --num_proc 8
-OPENAI_BASE_URL='http://localhost:8000/v1' python generate_answers.py --model_name 'augmxnt/shisa-7b-v1.rp1.15' --num_proc 8
-OPENAI_BASE_URL='http://localhost:8000/v1' python generate_answers.py --model_name 'shisa-ai/shisa-llama3-8b-v1.rp1.15' --num_proc 8
-OPENAI_BASE_URL='http://localhost:8000/v1' python generate_answers.py --model_name 'shisa-ai/shisa-yi1.5-9b-v1.rp1.15' --num_proc 8
-OPENAI_BASE_URL='http://localhost:8000/v1' python generate_answers.py --model_name 'shisa-ai/shisa-gemma-7b-v1.rp1.15' --num_proc 8
+# OPENAI_BASE_URL='http://localhost:8000/v1' python generate_answers.py --model_name 'augmxnt/shisa-gamma-7b-v1' --num_proc 8
+# OPENAI_BASE_URL='http://localhost:8000/v1' python generate_answers.py --model_name 'augmxnt/shisa-7b-v1' --num_proc 8
+OPENAI_BASE_URL='http://localhost:8000/v1' python generate_answers.py --model_name 'shisa-ai/shisa-llama3-8b-v1' --num_proc 8
+# OPENAI_BASE_URL='http://localhost:8000/v1' python generate_answers.py --model_name 'shisa-ai/shisa-yi1.5-9b-v1' --num_proc 8
+# OPENAI_BASE_URL='http://localhost:8000/v1' python generate_answers.py --model_name 'shisa-ai/shisa-gemma-7b-v1' --num_proc 8
 #OPENAI_BASE_URL='http://localhost:8000/v1' python generate_answers.py --model_name 'meta-llama/Meta-Llama-3-8B-Instruct' --num_proc 8
 # OPENAI_BASE_URL='http://localhost:8000/v1' python generate_answers.py --model_name 'meta-llama/Meta-Llama-3-70B-Instruct' --num_proc 8
